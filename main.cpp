bec6ed5e86df3c91f9541728f7ae2b17c1d76785
Frank Ngo <frank.ngo.1702@gmail.com>
2018-10-03 11:35:09 -0700

    clean up main

--- main.cpp
+++ main.cpp
@@ -5,11 +5,16 @@
 int main()
 {
 	vector<string> v;
-	string temp, output = "";
+	string temp, input_file, output_file;
 	int i = 0;
 	fstream f, g;
-	f.open("testcase.txt", ios::in);
-	g.open("output.txt", ios::out);
+
+	cout << "Enter name of input file (*.txt): ";
+	getline(cin, input_file);
+	cout << "Enter name of output file (*.txt): ";
+	getline(cin, output_file);
+	f.open(input_file, ios::in);
+	g.open(output_file, ios::out);
 
 	g << "Output:" << endl;
 	g << "token\t\tlexeme" << endl;
@@ -20,16 +25,13 @@ int main()
 		if (f.fail())
 			break;
 		v.push_back(temp);
+		g << lexer(temp) << endl;
 		++i;
-	}
-
-	for (i = 0; i < v.size(); ++i)
-	{
-		output = lexer(v.at(i));
-		g << output << endl;
 	}
 
 	f.close();
 	g.close();
+	cout << "Done... Please refer to " << output_file << endl;
+	system("pause");
 	return 0;
 }
