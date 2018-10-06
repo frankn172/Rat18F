751540ef714996eff67556f3a6004a525e00460b
Frank Ngo <frank.ngo.1702@gmail.com>
2018-10-06 00:14:58 -0700

    attempt to delete comments
    unsuccessful

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	vector<string> v;
 	string temp, input_file, output_file;
-	int i = 0;
+	int i = 0, counter = 0, start_point, end_point;
 	fstream f, g;
 
 	cout << "Enter name of input file (*.txt): ";
@@ -25,8 +25,33 @@ int main()
 		if (f.fail())
 			break;
 		v.push_back(temp);
-		g << lexer(temp) << endl;
 		++i;
+	}
+
+	while (true)
+	{
+		counter = 0;
+		for (i = 0; i < v.size(); ++i)
+		{
+			if (v.at(i).compare("[*") == 0)
+			{
+				start_point = i;
+				counter++;
+			}
+			if (v.at(i).compare("*]") == 0)
+			{
+				end_point = i;
+				counter++;
+			}
+		}
+		v.erase(v.begin() + start_point, v.begin() + end_point);
+		if (counter == 0)
+			break;
+	}
+
+	for (i = 0; i < v.size(); ++i)
+	{
+		g << v.at(i) << endl;
 	}
 
 	f.close();
